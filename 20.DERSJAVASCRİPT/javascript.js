cc784d0338fa9f0b820b94d106204b743eeff928
html3 <leon673421@gmail.com>
2023-12-06 22:43:43 +0300

    20.DERS

---
+++ 20.DERSJAVASCRİPT/javascript.js
@@ -0,0 +1,53 @@
+function myFunction3() {
+    document.getElementById("demo").innerHTML= "External Script çalışır"
+}
+
+// x,y,z varibles
+
+var x = 5;
+var y = 6;
+var z = x+y ;
+console.log(z)
+
+
+// const değişmeyen değerler için kullanılır
+
+ const price1 = 5;
+ const price2 = 6;
+ x = x + 5;
+ let total = price1 + price2
+// total = 40;
+
+ console.log(total)
+
+console.log(total)
+console.log(price1 == price1)
+
+const pi = 3.14 ;
+let person= "Mustafa"
+console.log(person)
+
+var carName1;
+let carName2;
+carNanme2 = "Volvo"
+let carName3 = "Mercedes"
+console.log(carName2)
+let person4 = "Ahmet" , carName4 = "Renault" , price4 = 200;
+
+
+let carName = "Volvo"
+var carName5 = "Fıat";
+var carName5;
+console.log(carName5)
+//    süslü parantez içinde letle okumaz varla okur
+a1=5;
+{
+    let a1 = 2 ;
+    console.log(a1)
+}
+console.log(a1)
+
+{
+    var a2 = 3;
+}
+console.log(a2)
